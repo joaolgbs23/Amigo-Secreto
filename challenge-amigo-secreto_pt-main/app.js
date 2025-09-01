022413ee8e697b7216d371ad4e35221164edcc62
joaolgbs23 <joaolgbs23@gmail.com>
2025-09-01 14:14:52 -0300

    base de código e funções adicionar amigo, atualizar lista e sortear amigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,14 +1,50 @@
 let pessoasAdicionadas = [];
+let pessoaSorteada;
 function adicionarAmigo(){
-    let NomeAdicionado = document.getElementById("amigo").value;// seleciona o valor que for digitado
-    if(NomeAdicionado !== "" ){
-        pessoasAdicionadas.push(NomeAdicionado);
+    let nomeAdicionado = document.getElementById("amigo").value;// seleciona o valor que for digitado
+    if(nomeAdicionado.length > 0){
+        pessoasAdicionadas.push(nomeAdicionado);
         console.log(pessoasAdicionadas);//mostra se foi adicionado corretamente
         let nomeNaLista = document.getElementById("listaAmigos");
         nomeNaLista.innerHTML = pessoasAdicionadas; // adiciona as pessoas visualmente a lista
     } else{
         alert("nome invalido");//caso invalido avisa o sistema
     }
+    limparCampo();
+    sortearAmigo();
+    
 }
-function atualizarlista(){}
-function sortearAmigos(){}
+function atualizarLista(){
+
+    
+}
+
+
+
+
+
+function sortearAmigo(){
+    if (pessoasAdicionadas.length > 0){
+            let sorteio = Math.floor(Math.random()* pessoasAdicionadas.length);
+            let pessoaSorteada = pessoasAdicionadas[sorteio];
+            console.log(sorteio);
+            pessoaSorteada = document.getElementById("resultado");
+            pessoaSorteada.innerHTML = pessoaSorteada;
+            console.log(pessoaSorteada);
+       
+
+
+    }
+
+}
+    
+    
+    /* Ninguém pode tirar a si mesmo.Ninguém pode ser sorteado por mais de uma pessoa (ou seja, o sorteio precisa ser justo e único).Todos terminam com um único "amigo secreto" designado.
+*/
+
+
+function limparCampo(){
+    nomeAdicionado = document.getElementById("amigo");
+    nomeAdicionado.value = ""
+
+}
